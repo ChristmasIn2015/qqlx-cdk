18d5b8154f0f2538998184471a2f512b0b1b1286
huangwenqiang <974879409@qq.com>
2023-04-12 23:44:42 +0800

    fixbugs

--- lib/function.time.ts
+++ lib/function.time.ts
@@ -92,38 +92,20 @@ export function getTime(mills: number) {
     };
 }
 
-// 网上复制的，获取中文金额
-export function getChineseMoney(n: number) {
-    var fraction = ["角", "分"];
-    var digit = ["零", "壹", "贰", "叁", "肆", "伍", "陆", "柒", "捌", "玖"];
-    var unit = [
-        ["元", "万", "亿"],
-        ["", "拾", "佰", "仟"],
-    ];
-    var head = n < 0 ? "欠" : "";
-    n = Math.abs(n);
+/** 仅用于查看函数耗时 */
+export function TimeLog(message: string) {
+    return function (target: Object | Function, propertyKey: string | symbol, descriptor: TypedPropertyDescriptor<any>) {
+        const original = descriptor.value;
 
-    var s = "";
+        descriptor.value = async function (...args: any[]) {
+            const start = Date.now();
+            const data = await original.apply(this, args);
 
-    for (var i = 0; i < fraction.length; i++) {
-        s += (digit[Math.floor(n * 10 * Math.pow(10, i)) % 10] + fraction[i]).replace(/零./, "");
-    }
-    s = s || "整";
-    n = Math.floor(n);
+            const gap = Date.now() - start;
+            console.log(message, gap, "\n");
+            return data;
+        };
 
-    for (var i = 0; i < unit[0].length && n > 0; i++) {
-        var p = "";
-        for (var j = 0; j < unit[1].length && n > 0; j++) {
-            p = digit[n % 10] + unit[1][j] + p;
-            n = Math.floor(n / 10);
-        }
-        s = p.replace(/(零.)*零$/, "").replace(/^$/, "零") + unit[0][i] + s;
-    }
-    return (
-        head +
-        s
-            .replace(/(零.)*零元/, "元")
-            .replace(/(零.)+/g, "零")
-            .replace(/^整$/, "零元整")
-    );
+        return descriptor;
+    };
 }

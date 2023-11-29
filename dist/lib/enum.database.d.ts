1279a49db107d41bb2525d61883eb44bcde85b7f
huangwenqiang <huangwenqiang@shoptop.com>
2023-11-29 14:27:32 +0800

    git reset cached

--- dist/lib/enum.database.d.ts
+++
@@ -1,6 +0,0 @@
-export declare enum MongodbSort {
-    /** 升序，从小到大 */
-    ASC = 1,
-    /** 降序，从大到小 */
-    DES = -1
-}
--- dist/lib/enum.time.d.ts
+++
@@ -1,7 +0,0 @@
-/** 某些分析数据需要按照时间间隔分布 */
-export declare enum TimeGap {
-    DAY = 0,
-    WEEK = 1,
-    MONTH = 2,
-    YEAR = 3
-}

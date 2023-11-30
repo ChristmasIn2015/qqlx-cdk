ab78fb8af2333f7617c89b4fa2010fe61e1e015b
huangwenqiang <huangwenqiang@shoptop.com>
2023-11-30 14:43:54 +0800

    updates

--- model/pond_log.ts
+++ model/pond_log.ts
@@ -7,34 +7,34 @@ import { RELATIONS_POND_LOG_NAME, ENUM_POND_LOG } from "qqlx-core"
 @Entity({ name: RELATIONS_POND_LOG_NAME })
 export class PondLogSchema implements PondLog {
 
-    @Column({ default: ENUM_POND_LOG.ALL })
+    @Column({ type: "smallint", default: ENUM_POND_LOG.ALL })
     type: ENUM_POND_LOG = ENUM_POND_LOG.ALL
 
-    @Column({ default: "" })
+    @Column({ type: 'varchar', length: 50, default: "" })
     title: VARCHAR50_PG = ""
 
-    @Column({ default: "" })
+    @Column({ type: 'varchar', default: "" })
     text: VARCHAR_PG = ""
 
-    @Column({ default: -1 })
+    @Column({ type: "bigint", default: -1 })
     duration: BIGINT_PG = -1
 
     // =============================
     // ======= 必须有的字段 ========
     // =============================
 
-    @PrimaryGeneratedColumn()
+    @PrimaryGeneratedColumn({ type: "integer" })
     id: number = -1
 
-    @Column({ default: "" })
+    @Column({ type: 'varchar', length: 50, default: "" })
     _id: VARCHAR50_PG = ""
 
-    @Column({ default: false })
+    @Column({ type: "boolean", default: false })
     isDisabled: boolean = false
 
-    @Column({ default: -1 })
+    @Column({ type: "bigint", default: -1 })
     timeCreate: BIGINT_PG = Date.now()
 
-    @Column({ default: -1 })
+    @Column({ type: "bigint", default: -1 })
     timeUpdate: BIGINT_PG = Date.now()
 }

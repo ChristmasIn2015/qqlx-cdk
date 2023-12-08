abbc8ce8d8e3498fdc442f940499fec8dce90e51
huangwenqiang <huangwenqiang@shoptop.com>
2023-12-08 13:51:47 +0800

    updates

--- lib/search.ts
+++ lib/search.ts
@@ -1,11 +1,53 @@
 import type {
     Page, PageRes, TimeFilter,
-    KeyString, KeySortable, KeyBigInt,
+    KeyString, KeySortable, KeyBigInt, KeyBool, KeyAccumulatable,
+    ConditionMatchStr, ConditionMatchInt, ConditionMatchBool,
     ConditionRegExp, ConditionSort, ConditionTime
 } from "qqlx-core"
 import { ConditionType, Sortable } from "qqlx-core"
 import { toString } from "./verify"
 
+// ========================== Condition ==========================
+
+export function getConditionMatchStr<T> (key: KeyString<T>): ConditionMatchStr<T> {
+    return {
+        type: ConditionType.MatchStr,
+        key: key,
+        value: ""
+    }
+}
+
+export function getConditionMatchInt<T> (key: KeyAccumulatable<T>): ConditionMatchInt<T> {
+    return {
+        type: ConditionType.MatchInt,
+        key: key,
+        value: 0
+    }
+}
+
+export function getConditionMatchBool<T> (key: KeyBool<T>): ConditionMatchBool<T> {
+    return {
+        type: ConditionType.MatchBool,
+        key: key,
+        value: false
+    }
+}
+
+export function getConditionRegExp<T> (key: KeyString<T>): ConditionRegExp<T> {
+    return {
+        type: ConditionType.RegExp,
+        key: key,
+        value: ""
+    }
+}
+
+export function getConditionSort<T> (key: KeySortable<T>): ConditionSort<T> {
+    return {
+        type: ConditionType.Sort,
+        key: key,
+        value: Sortable.DESC
+    }
+}
 
 export function getTimeFilter (): TimeFilter {
     return {
@@ -13,6 +55,16 @@ export function getTimeFilter (): TimeFilter {
         endTime: toString(Date.now()),
     };
 }
+
+export function getConditionTime<T> (key: KeyBigInt<T>): ConditionTime<T> {
+    return {
+        type: ConditionType.Time,
+        key: key,
+        value: getTimeFilter()
+    }
+}
+
+// ========================== Page ==========================
 
 /** pageSize 最多返回 1000 条结果 */
 export function getPageDto<T> (pageSize = 15): Page<T> {
@@ -32,30 +84,4 @@ export function getPageResDto<T> (): PageRes<T> {
         list: [],
         calcus: [],
     };
-}
-
-// ========================== Condition ==========================
-
-export function getConditionTime<T> (key: KeyBigInt<T>): ConditionTime<T> {
-    return {
-        type: ConditionType.Time,
-        key: key,
-        value: getTimeFilter()
-    }
-}
-
-export function getConditionRegExp<T> (key: KeyString<T>): ConditionRegExp<T> {
-    return {
-        type: ConditionType.RegExp,
-        key: key,
-        value: ""
-    }
-}
-
-export function getConditionSort<T> (key: KeySortable<T>): ConditionSort<T> {
-    return {
-        type: ConditionType.Sort,
-        key: key,
-        value: Sortable.DESC
-    }
 }

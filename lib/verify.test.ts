ae6bbe3f7dbefc47adf53d4e7a0d246d76894750
huangwenqiang <huangwenqiang@shoptop.com>
2024-01-05 18:28:55 +0800

    updates

--- lib/verify.test.ts
+++ lib/verify.test.ts
@@ -1,29 +1,7 @@
 import * as verify from "./verify"
 
 
-for (const mess of [
-    null,
-    undefined,
-    NaN,
-    "null",
-    "undefined",
-    "NaN",
-    "object",
-    10086,
-    0,
-    -1,
-    -10086,
-    "",
-    "10086",
-    "true",
-    "false",
-    true,
-    false,
-    {},
-    new Object,
-    new Map(),
-    function () { },
-]) {
+for (const mess of [...verify.INVALUE_VALUE_LIST, ...verify.DEFAULT_VALUE_LIST]) {
     describe(`[${typeof mess}] ${mess} testing...`, () => {
         it("isValid", () => {
             const result = verify.isValid(mess)
@@ -48,34 +26,11 @@ for (const mess of [
         it("toNumber", () => {
             const result = verify.toNumber(mess)
             expect(result).toEqual(expect.any(Number))
-
-            if ([null, undefined, NaN, "null", "undefined", "NaN"].includes(mess as any)) {
-                expect(result).toBe(-1)
-            } else {
-                expect([-10086, -1, 0, 10086]).toEqual(expect.arrayContaining([result]));
-            }
-        })
-        it("toNumber 5678", () => {
-            const result = verify.toNumber(mess, 5678)
-            expect(result).toEqual(expect.any(Number))
-
-            if ([null, undefined, NaN, "null", "undefined", "NaN"].includes(mess as any)) {
-                expect(result).toBe(5678)
-            } else {
-                expect([-10086, 0, 5678, 10086]).toEqual(expect.arrayContaining([result]));
-            }
+            expect(new RegExp(/^-?\d+(\.\d+)?([eE][-+]?\d+)?$/).test(result.toString())).toBe(true)
         })
         it("toString", () => {
             const result = verify.toString(mess)
             expect(result).toEqual(expect.any(String))
-        })
-        it("toString hello", () => {
-            const result = verify.toString(mess, "hello")
-            expect(result).toEqual(expect.any(String))
-
-            if ([null, undefined, NaN, "null", "undefined", "NaN"].includes(mess as any)) {
-                expect(result).toBe("")
-            }
         })
         it("toBoolean", () => {
             const result = verify.toBoolean(mess)
